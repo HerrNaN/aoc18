89b583c09264fb628ec446c0306e5732009342dc
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2018-12-02 22:47:13 +0100

    Remove redundand do statment

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,21 +12,19 @@ main = hspec $ do
         it "Part A" $ day01a [1, -2, 3, 1] `shouldBe` 3
         it "Part B" $ day01b [1, -2, 3, 1] `shouldBe` 2
     describe "Day 2" $ do
-        it "Part A" $ do
-            day02a [ "abcdef"
-                   , "bababc"
-                   , "abbcde"
-                   , "abcccd"
-                   , "aabcdd"
-                   , "abcdee"
-                   , "ababab"
-                   ] `shouldBe` 12
-        it "Part B" $ do
-            day02b [ "abcde"
-                   , "fghij"
-                   , "klmno"
-                   , "pqrst"
-                   , "fguij"
-                   , "axcye"
-                   , "wvxyz"
-                   ] `shouldBe` "fgij"
+        it "Part A" $ day02a [ "abcdef"
+                             , "bababc"
+                             , "abbcde"
+                             , "abcccd"
+                             , "aabcdd"
+                             , "abcdee"
+                             , "ababab"
+                             ] `shouldBe` 12
+        it "Part B" $ day02b [ "abcde"
+                             , "fghij"
+                             , "klmno"
+                             , "pqrst"
+                             , "fguij"
+                             , "axcye"
+                             , "wvxyz"
+                             ] `shouldBe` "fgij"
