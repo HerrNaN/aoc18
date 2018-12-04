0e6f26e57505fd4bd9d6207137949ddbf2a7e102
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2018-12-04 21:01:38 +0100

    Add solutions to Day 4

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,7 +1,8 @@
 module Day04 (day04a, day04b) where
 
-import Data.List
-import Data.Map.Strict (Map, adjust, keys, elems, empty, (!))
+import Data.List hiding (insert)
+import Data.Map.Strict hiding (map, drop, take)
+import qualified Data.Map.Strict as M
 import Data.Ord (comparing)
 import Data.Function (on)
 
@@ -24,24 +25,34 @@ readEntry s | "falls asleep" `isInfixOf` s = Entry (read(minutes s) :: Int) S
 readAllEntries :: [String] -> [Entry]
 readAllEntries = map readEntry
 
-sleepyG :: [Entry] -> Int -> Map Int (Map Int Int) -> (Int,Int)
-sleepyG []                        _ m = values m
+sleepyG :: [Entry] -> Int -> Map Int (Map Int Int) -> Map Int (Map Int Int)
+sleepyG []                        _ m = m
 sleepyG (Entry n B:es)            c m = sleepyG es n m
 sleepyG (Entry n S:Entry n' W:es) c m = sleepyG es c m'
-    where m'  = adjust (incValuesWithKeys [n..n']) c m
+    where m'  = insert c (incValuesWithKeys [n..(n'-1)] v') m
+          v   = m !? c
+          v'  = case v of
+                     Just x -> x
+                     _      -> empty
 
 incValuesWithKeys :: [Int] -> Map Int Int -> Map Int Int
-incValuesWithKeys [] m = m
-incValuesWithKeys ks m = foldl (flip (adjust (1 +))) m ks
+incValuesWithKeys []     m = m
+incValuesWithKeys (k:ks) m = incValuesWithKeys ks $ insertWith inc k 1 m
+    where inc n o = o + n
 
 values :: Map Int (Map Int Int) -> (Int, Int)
 values m = (guard, minute)
-    where (guard, _)  = maximumBy (compare `on` snd) [(g,sum $ elems ms) | (g,ms) <- zip (keys m) (elems m)]
-          (minute, _) = maximumBy (compare `on` snd) $ zip (keys ts) (elems ts)
-          ts          = m ! guard
-          
+    where (guard, _)  = maximumBy (compare `on` snd) [(g, sum $ elems $ m ! g) | g <- keys m]
+          (minute, _) = maximumBy (compare `on` snd) $ zipMap $ m ! guard
+          zipMap a    = zip (keys a) (elems a)
+
+idMin :: Map Int (Map Int Int) -> (Int, Int)
+idMin m = (id, min)
+    where m' = M.map (maximumBy (compare `on` snd) . M.toList) m
+          (id, (min, _)) = maximumBy (compare `on` (snd . snd)) $ M.toList m'
 
 day04a :: String -> Int
-day04a s = uncurry (*) $ sleepyG (readAllEntries $ parseInput s) 0 empty
+day04a s = uncurry (*) $ values $ sleepyG (readAllEntries $ parseInput s) 0 empty
 
-day04b = undefined
+day04b :: String -> Int
+day04b s = uncurry (*) $ idMin $ sleepyG (readAllEntries $ parseInput s) 0 empty
--- test/Spec.hs
+++ test/Spec.hs
@@ -33,8 +33,11 @@ main = hspec $ do
     describe "Day 3" $ do
         it "Part A" $ day03a "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2" `shouldBe` 4
         it "Part B" $ day03b "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2" `shouldBe` 3
-    describe "Day 4" $ do
+    describe "Day 4" $ do 
+        
         it "Part A" $ do
             example <- readFile "example04.txt"
             day04a example `shouldBe` 240
-        it "Part B" $ day04b `shouldBe` ()
+        it "Part B" $ do
+            example <- readFile "example04.txt"
+            day04b example `shouldBe` 4455
